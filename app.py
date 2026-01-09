03934c30fc000411e569ad76e77420bb0eeeeeab
Abhi <johnabhi744@gmail.com>
2026-01-09 19:04:22 +0530

    model is linked with the fastAPI

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field ,computed_field
 from typing import Literal, Annotated
 import pickle
+import pandas as pd
+from fastapi.responses import JSONResponse
+
 
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
@@ -101,4 +104,20 @@ class UserInput(BaseModel):
             return 2 
         else :
             return 3
-        
+
+
+
+@app.post('/predict')
+def predict_premium(data:UserInput):
+    input_df=pd.DataFrame([{
+        'bmi':data.bmi,
+        'age_group':data.age_group,
+        'lifestyle_risk':data.lifestyle_risk,
+        'city_tier':data.city_tier,
+        'income_lpa':data.income_lpa,
+        'occupation':data.occupation
+    }])
+
+    prediction=model.predict(input_df)[0]
+
+    return JSONResponse(status_code=200,content={'predicted_category':prediction})
